4f3f39f1452da864c8b50d610e20b0a4da80ca19
Aayushbharti <103554027+Aayushbharti@users.noreply.github.com>
2023-08-27 11:04:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const hamb = document.querySelector(".hamb");
+const navList = document.querySelector(".menu-overlay");
+
+hamb.addEventListener("click", function() {
+	navList.classList.toggle("active");
+	hamb.classList.toggle("click");
+}
+) 
